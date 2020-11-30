dd4c61f9efbeaf80bb4d33d0b3e79a7ee5e190b7
mbidkham <mh.bidkham@gmail.com>
2020-11-30 21:50:43 +0300

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/me/test/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.me.test.security;
+
+import com.me.test.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
